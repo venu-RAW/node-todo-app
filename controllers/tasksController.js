d43373cb96c6c9329321c3c68bcf694ec3d600b1
venu-RAW <venu.sirpuram@raweng.com>
2020-12-14 22:22:32 +0530

    completed the crud operations for the task

--- controllers/tasksController.js
+++ controllers/tasksController.js
@@ -1,12 +1,11 @@
-const mongoose = require("mongoose");
 const Task = require("../models/taskSchema.js");
 
-const getTasks = async (req, res, next) => {
+const getTasks = async (req, res) => {
 	const allTasks = await Task.find();
 	res.send(allTasks);
 };
 
-const addTasks = async (req, res, next) => {
+const addTasks = async (req, res) => {
 	const { taskName } = req.body;
 
 	let newtask = new Task({ taskName });
@@ -14,13 +13,52 @@ const addTasks = async (req, res, next) => {
 	try {
 		newTask = await newtask.save();
 	} catch (err) {
-		console.log(err);
+		console.error(err);
 	}
 
 	res.send("data added successfully");
 };
 
+const getTaskById = async (req, res) => {
+	const { taskId } = req.params;
+
+	try {
+		let task = await Task.find({ taskId });
+		res.send(task);
+	} catch (err) {
+		console.error(err);
+	}
+};
+
+const updateTasks = async (req, res) => {
+	const { taskId } = req.params;
+
+	try {
+		let task = await Task.updateOne(
+			{ taskId: taskId },
+			{ $set: { taskName: req.body.taskName } }
+		);
+		res.send(task);
+	} catch (err) {
+		console.error(err);
+	}
+};
+
+const deleteTaskById = async (req, res) => {
+	const { taskId } = req.params;
+
+	try {
+		await Task.deleteOne({ taskId });
+		res.send("task deleted successfully");
+	} catch (err) {
+		console.error(err);
+	}
+};
+
 module.exports = {
 	getTasks,
 	addTasks,
+	updateTasks,
+	getTaskById,
+	deleteTaskById,
 };
--- routes/taskRoute.js
+++ routes/taskRoute.js
@@ -1,8 +1,19 @@
 const router = require("express").Router();
 
-const { getTasks, addTasks } = require("../controllers/tasksController.js");
+const {
+	getTasks,
+	addTasks,
+	updateTasks,
+	getTaskById,
+	deleteTaskById,
+} = require("../controllers/tasksController.js");
 
-router.route("/tasks").get(getTasks);
-router.route("/tasks").post(addTasks);
+router.route("/tasks").get(getTasks).post(addTasks);
+
+router
+	.route("/tasks/:taskId")
+	.get(getTaskById)
+	.put(updateTasks)
+	.delete(deleteTaskById);
 
 module.exports = router;
